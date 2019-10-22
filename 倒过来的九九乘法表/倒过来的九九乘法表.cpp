dc8288c0970380ee8a5ee58f259fea3a18f24652
20191864233 <Obi1937@live.com>
2019-10-22 15:56:19 +0800

    倒过来的九九乘法表（笨办法）

---
+++ 倒过来的九九乘法表/倒过来的九九乘法表.cpp
@@ -0,0 +1,23 @@
+// 倒过来的九九乘法表.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{   
+	for (int a=1;a<=9;a++)
+	{    
+		for (int b=1;b<=9;b++)
+		{
+			if (b>=a)
+				printf("%d*%d=%-2d ",b,a,a*b);
+			else
+				printf("       ");
+		}
+		printf("\n");
+	}
+	system("pause");
+	return 0;
+}
