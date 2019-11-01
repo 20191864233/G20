7642a22d687b868a89c0d1d102ef27297b74b38c
20191864233 <Obi1937@live.com>
2019-11-01 10:34:02 +0800

    修改4.3，4.12开了个头

---
+++ c4/4.12/test12.cpp
@@ -0,0 +1,12 @@
+// test12.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+
+
+int main(int argc, char* argv[])
+{
+	
+	return 0;
+}
--- c4/4.4/test4.cpp
+++ c4/4.4/test4.cpp
@@ -5,27 +5,33 @@
 #include <stdio.h>
 #include <iostream>
 
-void larger();
+void largest(int a,int b,int c);
 
 int main(int argc, char* argv[])
 {
-	larger();
+	int x,y,z;
+	printf ("输入a：");
+	    scanf ("%d",&x);
+    printf ("输入b：");
+        scanf ("%d",&y);
+    printf ("输入c：");
+        scanf ("%d",&z);
+
+	largest(x,y,z);
 
 	system("pause");
 	return 0;
 }
 
-void larger()
-{
-    int a,b,c,max,max1;
-	    printf("输入a：");
-	        scanf("%d",&a);
-        printf("输入b：");
-        	scanf("%d",&b);
-        printf("输入c：");
-        	scanf("%d",&c);
-    	max1=(a>b) ? a:b;
-    	max=(max1>c) ? max1:c;
-	printf("最大值为：%d\n",max);
 
+
+
+void largest(int a,int b,int c)
+{
+    int max,max1;
+
+    max1=(a>b) ? a:b;
+    max=(max1>c) ? max1:c;
+
+	printf("最大值为：%d\n",max);
 }
