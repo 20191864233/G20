331ed8b8553578f1f28dae7162be0779c1e51d42
20191864233 <Obi1937@live.com>
2019-11-03 18:04:58 +0800

    4.9简化（孙泽霖的版本）

--- c4/4.9/test9.cpp
+++ c4/4.9/test9.cpp
@@ -4,151 +4,73 @@
 #include "stdafx.h"
 #include <stdio.h>
 #include <iostream>
-
-void jiweishu(int x);    //计算位数
-void shuchushuzi(int x);    //输出每一位数字
-void nixu(int x);    //逆序输出
+#include <stdlib.h>
+void weishu(int a);
+void meiyiweishu(int a);
+void nixv(int a);
 
 
 int main(int argc, char* argv[])
 {
 	for(;;)
 	{
-        int a;
+        int x;
 	    printf("输入一个不多于五位的正整数：");
-	    scanf("%d",&a);
+	    scanf("%d",&x);
 
-	    if (a>=1 && a<=99999)
+	    if (x>=1 && x<=99999)
 		{
-		    jiweishu(a);    //计算位数
-		    shuchushuzi(a);    //输出每一位数字 
-		    nixu(a);    //逆序输出
+			weishu(x);
+            meiyiweishu(x);
+             printf("\n");
+            nixv(x);
 			break;
 		}
 		else
-			printf("错误，请输入符合要求的数\n");
+			printf("error!");
 	}
-
 	system("pause");
 	return 0;
-}
-
-
-
-
-
-void jiweishu(int x)    //计算位数
-{
-	printf("(1)");
-
-	if (x>=1 && x<=9)
-		printf("一位数\n");
-	if (x>=10 && x<=99)
-		printf("两位数\n");
-	if (x>=100 && x<=999)
-		printf("三位数\n");
-	if (x>=1000 && x<=9999)
-		printf("四位数\n");
-	if (x>=10000 && x<=99999)
-		printf("五位数\n");
-}
-
-
-
-void shuchushuzi(int x)    //输出每一位数字
-{
-	printf("(2)");
-
-	if (x>=1 && x<=9)
-		printf("%d",x);
-    if (x>=10 && x<=99)
-	{
-		int x1,x2;
-		x1=x/10;
-		x2=x%10;
-		printf("输出每一位数字：%d  %d\n",x1,x2);
 	}
-    if (x>=100 && x<=999)
-	{
-		int x1,x1a,x2,x3;
-		x1=x/100;
-		x1a=x%100;
-		x2=x1a/10;
-		x3=x1a%10;
-		printf("输出每一位数字：%d  %d  %d\n",x1,x2,x3);
-	}
-    if (x>=1000 && x<=9999)
-	{
-		int x1,x1a,x2,x2a,x3,x4;
-		x1=x/1000;
-		x1a=x%1000;
-		x2=x1a/100;
-		x2a=x1a%100;
-		x3=x2a/10;
-		x4=x2a%10;
-		printf("输出每一位数字：%d  %d  %d  %d\n",x1,x2,x3,x4);
-	}
-    if (x>=10000 && x<=99999)
-	{
-		int x1,x1a,x2,x2a,x3,x3a,x4,x5;
-		x1=x/10000;
-		x1a=x%10000;
-		x2=x1a/1000;
-		x2a=x1a%1000;
-		x3=x2a/100;
-		x3a=x2a%100;
-		x4=x3a/10;
-		x5=x3a%10;
-		printf("输出每一位数字：%d  %d  %d  %d  %d\n",x1,x2,x3,x4,x5);
-	}
-}
 
 
 
-void nixu(int x)    //逆序输出
-{
-	printf("(3)");
 
-	if (x>=1 && x<=9)
-		printf("%d",x);
-    if (x>=10 && x<=99)
+
+
+
+	void weishu(int a)
 	{
-		int x1,x2;
-		x1=x/10;
-		x2=x%10;
-		printf("逆序输出：%d%d\n",x2,x1);
+		int m=0;
+	while(a){
+        m++;
+        a/=10;
+		}
+		printf("%d\n",m);
 	}
-    if (x>=100 && x<=999)
+
+
+
+
+	void meiyiweishu (int a)
 	{
-		int x1,x1a,x2,x3;
-		x1=x/100;
-		x1a=x%100;
-		x2=x1a/10;
-		x3=x1a%10;
-		printf("逆序输出：%d%d%d\n",x3,x2,x1);
+	 int v;
+    if(a==0) return;
+    v=a%10;
+    meiyiweishu(a/10);
+    printf("%d ", v);
 	}
-    if (x>=1000 && x<=9999)
+
+
+
+
+
+
+	void nixv(int a)
 	{
-		int x1,x1a,x2,x2a,x3,x4;
-		x1=x/1000;
-		x1a=x%1000;
-		x2=x1a/100;
-		x2a=x1a%100;
-		x3=x2a/10;
-		x4=x2a%10;
-		printf("逆序输出：%d%d%d%d\n",x4,x3,x2,x1);
+		 while(a != 0)
+	  {
+         printf("%d",a%10);
+           a /= 10;
+	  }
 	}
-    if (x>=10000 && x<=99999)
-	{
-		int x1,x1a,x2,x2a,x3,x3a,x4,x5;
-		x1=x/10000;
-		x1a=x%10000;
-		x2=x1a/1000;
-		x2a=x1a%1000;
-		x3=x2a/100;
-		x3a=x2a%100;
-		x4=x3a/10;
-		x5=x3a%10;
-		printf("逆序输出：%d%d%d%d%d\n",x5,x4,x3,x2,x1);
-	}
-}
