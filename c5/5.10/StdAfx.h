9e3146632e991daacbcd9ced938480502acd3a39
20191864233 <Obi1937@live.com>
2019-11-11 20:29:53 +0800

    完成5.9，5.10在想

---
+++ c5/5.10/StdAfx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__FBAFA84B_6932_4343_BE25_CEB091F5E5B0__INCLUDED_)
+#define AFX_STDAFX_H__FBAFA84B_6932_4343_BE25_CEB091F5E5B0__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__FBAFA84B_6932_4343_BE25_CEB091F5E5B0__INCLUDED_)
--- c5/5.9/test9.cpp
+++ c5/5.9/test9.cpp
@@ -8,7 +8,28 @@
 int main(int argc, char* argv[])
 {
 	int x=1;
+	do
 	{
+		int y,s=0;
+		for(y=1;y<=x/2;y++)
+		{
+			if (x%y==0)
+				s=s+y;
+		}
+		if (s==x)
+		{
+			printf("%d its factors are ",x);
+			int z=1;
+			while (z<=x/2)
+			{
+				if (x%z==0)
+					printf("%d,",z);
+				z++;
+			}
+			printf("\b \n");
+		}
+
+		x++;
 	}while (x<=1000);
 	return 0;
 }
