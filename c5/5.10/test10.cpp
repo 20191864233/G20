6345e8fe626f4cc7a56905c9d9b4366a4823d5d3
20191864233 <Obi1937@live.com>
2019-11-11 21:09:06 +0800

    完成5.1

--- c5/5.10/test10.cpp
+++ c5/5.10/test10.cpp
@@ -7,5 +7,19 @@
 
 int main(int argc, char* argv[])
 {
+	int x=2,y=1;
+	int a;
+	int n=1;
+	float s=0;
+
+	do
+	{
+		s=s+x/y;
+		a=x;
+		x=x+y;
+		y=a;
+		n++;
+	}while (n<=20);
+	printf("前20项之和：%f\n",s);
 	return 0;
 }
