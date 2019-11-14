91bac09d7c9f72e9743131cc8a9881df1c8615fe
20191864233 <Obi1937@live.com>
2019-11-14 20:32:30 +0800

    求pi(法7暂时不会)

---
+++ c6/test1/StdAfx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__6BADF2B4_D7F1_4A43_88FF_707C055F0564__INCLUDED_)
+#define AFX_STDAFX_H__6BADF2B4_D7F1_4A43_88FF_707C055F0564__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__6BADF2B4_D7F1_4A43_88FF_707C055F0564__INCLUDED_)
--- pi/pi.cpp
+++ pi/pi.cpp
@@ -12,6 +12,10 @@ void pi1();
 void pi2();
 void pi3();
 void pi4();
+void pi5();
+void pi6();
+void pi7();
+void pi8();
 
 
 int main(int argc, char* argv[])
@@ -20,6 +24,10 @@ int main(int argc, char* argv[])
 	pi2();
 	pi3();
 	pi4();
+	pi5();
+	pi6();
+	pi7();
+	pi8();
 
 	return 0;
 }
@@ -64,16 +72,19 @@ void pi2()
 
 void pi3()
 {
-	double pi=2,x1=1,x2=1;
+	double pi=2,x1=1,x2=1,x3,x4;
 	int n=0;
 	do
 	{
 		pi=pi*2/sqrt(x1+x2);
 
+		x3=x1;
+		x4=x2;
+
 		x2=sqrt(x1+x2);
 		x1=2;
 		n++;
-	}while( fabs() >= pow(10,-5) );
+	}while( fabs(2/sqrt(x1+x2)-2/sqrt(x3+x4)) >= pow(10,-5) );
 
 	printf("法3：pi=%.14f，次数：%d\n",pi,n);
 }
@@ -82,21 +93,106 @@ void pi3()
 
 void pi4()
 {
-	double pi,a=1,x=2,y=1;
+	double pi,a=1,x1=2,y1=1,x0,y0;
 	int n=0;
 	do
 	{
-		a=a*(x/y);
+		a=a*(x1/y1);
+
+		x0=x1;
+		y0=y1;
+
+		if (n%2==0)
+			y1+=2;
+		else
+			x1+=2;
 
 		n++;
-		if (n!=1 && n%2==0)
-			y+=2;
-		else
-			x+=2;
-	}while(n<pow(10,6));
+	}while(fabs(x1/y1-x0/y0)>pow(10,-5));
 	pi=a*2;
 
 	printf("法4：pi=%.14f，次数：%d\n",pi,n);
 }
 
 
+
+void pi5()
+{
+	double pi,a=1,b=1,b0,x=1,y=3,t=3;
+	int n=0;
+	do
+	{
+		b0=b;
+		for(y;y<=t;y+=2)
+		{
+			b=b*(x/y);
+			x++;
+		}
+		a=a+b;
+
+		t+=2;
+		n++;
+	}while(fabs(b-b0)>=pow(10,-5));
+	pi=2*a;
+
+	printf("法5：pi=%.14f，次数：%d\n",pi,n);
+}
+
+
+
+
+void pi6()
+{
+	double pi,a=0,x=1;
+	int n=0;
+	
+	do
+	{
+		n++;
+
+		if (n%2==0)
+			a=a-1/x;
+		else
+			a=a+1/x;
+
+		x+=2;
+	}while( fabs( 1/x-1/(x-2) )>=pow(10,-5) );
+	pi=4*a;
+
+	printf("法6：pi=%.14f，次数：%d\n",pi,n);
+}
+
+
+
+
+void pi7()
+{
+	double pi,a=1/2,x=2,y=3;
+	int n=1;
+	do
+	{
+		
+	}while();
+	
+	printf("法7：pi=%.14f，次数：%d\n",pi,n);
+}
+
+
+
+
+void pi8()
+{
+	double pi,x=1,a=0;
+	int n=0;
+
+	do
+	{
+		a=a+1/pow(x,2);
+
+		x+=2;
+		n++;
+	}while( fabs( 1/pow(x,2)-1/pow(x-2,2) )>=pow(10,-5) );
+	pi=sqrt(8*a);
+	
+	printf("法8：pi=%.14f，次数：%d\n",pi,n);
+}
