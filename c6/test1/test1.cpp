15a364fdf9b967ad1f3d58c388a721c04f9a9d23
20191864233 <Obi1937@live.com>
2019-11-17 21:46:26 +0800

    完成5章

--- c6/test1/test1.cpp
+++ c6/test1/test1.cpp
@@ -1,18 +1,33 @@
 // test1.cpp : Defines the entry point for the console application.
 //
 
+#include "stdafx.h"
 #include <stdio.h>
+#include <math.h>
 
 
 int main()
 {
-	int i,a[10];
+	int i,a[100];
 
-	for(i=0;i<=9;i++);
-		a[i]=i;
-
-	for(i=9;i>=0;i--);
-		printf("%d",a[i]);
+	for(i=0;i<=99;i++)
+	{
+		a[i]=i+1;
+		if (a[i]%2!=0)
+		{
+			int r=0;
+			for (int x=3;x<=sqrt(a[i]);x+=2)
+			{
+				if (a[i]%x==0)
+				{
+					r=1;
+					break;
+				}
+			}
+			if (r=0)
+				printf("%d  ",a[i]);
+		}
+	}
 
 	printf("\n");
 	return 0;
