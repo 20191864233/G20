3026ecec89ef0b990b0a17932033cc4b0d0ca3c8
20191864233 <Obi1937@live.com>
2020-02-21 17:05:36 +0800

    first java

---
+++ c/hello world/StdAfx.h
@@ -0,0 +1,22 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__DF6A99F1_F585_49A8_9D1B_2F0CF99A88AF__INCLUDED_)
+#define AFX_STDAFX_H__DF6A99F1_F585_49A8_9D1B_2F0CF99A88AF__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <stdio.h>
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__DF6A99F1_F585_49A8_9D1B_2F0CF99A88AF__INCLUDED_)
