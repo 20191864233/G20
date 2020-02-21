fb87f907541d1b5fffc34f306f65c65720ad614f
20191864233 <Obi1937@live.com>
2020-02-21 17:06:39 +0800

    first java

---
+++ c/带方块/StdAfx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__EF27252B_7CC9_4567_94FA_C1028C3BA87B__INCLUDED_)
+#define AFX_STDAFX_H__EF27252B_7CC9_4567_94FA_C1028C3BA87B__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__EF27252B_7CC9_4567_94FA_C1028C3BA87B__INCLUDED_)
